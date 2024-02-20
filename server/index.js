0235e2240e696346b402c50d067704b3ba7dd415
Shridhar Kandikatla <shridharkandikatla@gmail.com>
2024-02-20 12:55:53 +0530

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
 const app = express();
 
 app.listen(5000, () => {

21425a02392db52d03d56b01b1c101bba5cd75eb
Shridhar Kandikatla <shridharkandikatla@gmail.com>
2024-02-20 11:49:31 +0530

    installation of react and tailwind css

---
+++ src/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <nav className='w-full h-16 flex bg-slate-400'>
+        <p>Sahaydri Real Estate</p>
+        <input type='text' placeholder='Search...' />
+        <p>Home</p>
+        <p>About</p>
+        <p>Sign in</p>
+      </nav>
+    </div>
+  );
+};
+
+export default Homepage;

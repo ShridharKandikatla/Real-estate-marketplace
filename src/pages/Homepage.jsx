461168a6da7e8c475cc7487515863a81a21f76e7
Shridhar Kandikatla <shridharkandikatla@gmail.com>
2024-02-20 12:32:26 +0530

    created header component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,13 +3,7 @@ import React from 'react';
 const Homepage = () => {
   return (
     <div>
-      <nav className='w-full h-16 flex bg-slate-400'>
-        <p>Sahaydri Real Estate</p>
-        <input type='text' placeholder='Search...' />
-        <p>Home</p>
-        <p>About</p>
-        <p>Sign in</p>
-      </nav>
+Home
     </div>
   );
 };
